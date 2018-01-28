2d60e8b46ea80a7eb7289e66020d084cfa38a4fd
gokel166 <okellogeorge40@yahoo.com>
2018-01-28 01:51:48 -0600

    modify components for clubs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Club } from './club';
 
 import { LocalStorageService } from './local-storage.service';
+import { ClubsService } from './clubs.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,10 @@ import { LocalStorageService } from './local-storage.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'The first angular app';
+  constructor(private clubService: ClubsService) {}
 
+  ngOnInit() {
+    this.clubService.clubber;
+    console.log(this.clubService.clubber)
+  }
 }
--- src/app/clubs.service.ts
+++ src/app/clubs.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { InMemClubService } from 'angular-in-memory-web-api';
 
 @Injectable()
 export class ClubsService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient){}
 
   clubber = [
     'Blue', 'Red', 'Green', 'Orange'
